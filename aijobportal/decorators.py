a7bdd78bce2dcb905d4d5d8e6951a5026bdec7f2
divyafelix <divyafelix@gmail.com>
2026-06-17 09:29:58 +0530

    Updated jobseeker registration, session handling, and navbar fix

--- aijobportal/decorators.py
+++ aijobportal/decorators.py
@@ -1,6 +1,13 @@
 from django.shortcuts import redirect
 from django.contrib import messages
 
+def wrapper(view_func):
+    def inner(request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return redirect('/admin/')
+        return view_func(request, *args, **kwargs)
+
+    return inner
 def company_required(view_func):
     def wrapper(request, *args, **kwargs):
         if request.user.is_authenticated and request.user.user_type == 'company':
